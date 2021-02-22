b3f7a8afe3268951f0e61941d779367adfc98f8f
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-22 09:54:24 -0800

    add good fail for TDD
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

---
+++ lib/ping-pong.rb
@@ -0,0 +1,4 @@
+input_number = 3
+def ping_pong(input_number)
+# some code
+end
--- spec/ping-pong_spec.rb
+++ spec/ping-pong_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
 require('ping-pong')
 
-describe('#ping-pong') do
+describe('#ping_pong') do
   it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(3)).to(eq([1,2,3]))
-  end
+  end 
+end
 
-  
