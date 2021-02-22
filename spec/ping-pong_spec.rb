14f584012009a3bd7d4c623ead7d0de35b9b70de
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-22 10:19:45 -0800

    add good fail for divisible by 5 test
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- spec/ping-pong_spec.rb
+++ spec/ping-pong_spec.rb
@@ -4,9 +4,13 @@ require('ping-pong')
 describe('#ping_pong') do
   it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(2)).to(eq([1,2]))
-  end 
+  end
 
   it("return an array with numbers divisible by 3 replaced with 'ping'") do
-    expect(ping_pong(5)).to(eq([1,2,'ping',4,5]))
+    expect(ping_pong(4)).to(eq([1,2,'ping',4]))
+  end
+
+  it("return an array with numbers divisble by 5 replaced with 'pong'") do
+    expect(ping_pong(5)).to(eq([1,2,'ping',4,'pong']))
   end
 end
