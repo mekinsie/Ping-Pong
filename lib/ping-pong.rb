ed38e55775140dc0fcdddb9c6203c0c180fcf5a2
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-22 10:36:55 -0800

    add passing test for divisible by 5 test
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- lib/ping-pong.rb
+++ lib/ping-pong.rb
@@ -6,6 +6,8 @@ def ping_pong(input_number)
     x += 1
     if(x % 3 == 0)
       ping_pong_array.push('ping')
+    elsif(x % 5 == 0)
+      ping_pong_array.push('pong')
     else
       ping_pong_array.push(x)
     end
