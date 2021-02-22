6afc33f8d4780a5387ca80988828ee300ec6f0d7
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-22 09:46:05 -0800

    setup ruby TDD and write first test
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

---
+++ spec/ping-pong_spec.rb
@@ -0,0 +1,9 @@
+require('rspec')
+require('ping-pong')
+
+describe('#ping-pong') do
+  it("returns an array of ascending numbers up to the number entered") do
+    expect(ping_pong(3)).to(eq([1,2,3]))
+  end
+
+  
